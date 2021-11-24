fbe08a05a687022d31ca21b83ad94e5347e476fa
Richard <richardjimenez.9641@gmail.com>
2021-11-24 09:58:12 -0500

    Reaload

--- assets/js/index.ts
+++
@@ -1,6 +0,0 @@
-import MediaPlayer from "./moProto.js";
-
-const video = document.querySelector("video");
-const button = document.querySelector("button");
-const player = new MediaPlayer({ el: video })
-button.onclick = () => player.togglePlay();
